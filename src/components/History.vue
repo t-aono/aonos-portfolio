66beb4b81db78ecf0d6a8a3bac0f5683f79b6ff3
tkhs <spain3405@gmail.com>
2021-08-04 08:48:56 +0900

    make history

--- src/components/History.vue
+++ src/components/History.vue
@@ -1,8 +1,8 @@
 <template>
-  <v-row class="text-center sub-color white--text py-10">
+  <v-row class="text-center sub-color white--text">
     <v-col cols="12">
-      <div class="text-h5 mb-0 main-color-txt" id="history">history</div>
-      <div class="text-h4 mb-5">経歴</div>
+      <h3 class="mb-0 main-color-txt" id="history">history</h3>
+      <h2 class="mb-5">経歴</h2>
       <v-container>
         <v-timeline :dense="sp">
           <v-timeline-item
@@ -17,12 +17,6 @@
               ></span>
             </template>
             <div class="py-4">
-              <div v-show="sp" class="mb-3">
-                <span
-                  class="headline font-weight-bold white--text"
-                  v-text="year.year">
-                </span>
-              </div>
               <h2 class="headline font-weight-light mb-4 white--text">
                 {{ year.title }}
               </h2>
@@ -45,36 +39,41 @@ export default {
     years: [
       {
         year: "2011",
-        title: "新卒で営業職",
-        sentence: "新卒で製薬メーカーに営業職入社。医療施設に対して自社医薬品の営業活動。2016年3月で退職。",
+        title: "営業職としてメーカー入社",
+        sentence: "新卒で製薬メーカーに営業職入社。病院、クリニック、調剤薬局などを対象に自社医薬品の営業活動。2016年3月で退職。",
       },
       {
         year: "2016",
         title: "SESの会社に入社",
-        sentence: "プログラミングやWebサーバー、データベースについて学ぶ。その後、既存システムのカスタマイズ案件でテスト仕様やテストなどを担当。",
+        sentence: "プログラミングのスクールに通い、Java・SQL・データベース・ウェブサーバーについて学ぶ。その後、テスターとして小売業向け業務支援システムのカスタマイズ案件に参画。",
       },
       {
         year: "2017",
-        title: "複数の開発現場を経験",
-        sentence: "Java や VBA がメインの保守開発案件で実装やテストなどを担当。",
+        title: "複数の開発現場でチーム開発を経験",
+        sentence: "プログラマーとして Java や VBA の案件に参画。",
       },
       {
         year: "2018",
-        title: "PHPでの開発を経験",
-        sentence: "既存Webシステムのカスタマイズ案件で画面設計、実装、テストなどを担当。",
+        title: "",
+        sentence: "",
+      },
+      {
+        year: "2019",
+        title: "",
+        sentence: "",
       },
       {
         year: "2020",
-        title: "受託開発の会社に転職",
-        sentence: "PHP / JavaScript を使ったWebシステムの開発を担当。お客さんとの打ち合わせから画面設計、DB設計、実装、テストなどを担当。",
+        title: "",
+        sentence: "",
       },
       {
         year: "2021",
-        title: "副業で実績を積む",
-        sentence: "ココナラ、クラウドワークスを利用してプログラムやWebシステムの制作を受注。対応数は100件以上。",
+        title: "",
+        sentence: "",
       },
     ],
-    sp: (window.innerWidth < 700) ? true : false,
+    sp: false,
   }),
   methods: {
     handleResize: function() {
@@ -83,7 +82,7 @@ export default {
     }
   },
   mounted: function () {
-    window.addEventListener('resize', this.handleResize);
+    window.addEventListener('resize', this.handleResize)
   },
 };
 </script>
@@ -91,6 +90,8 @@ export default {
 <style scoped>
 .v-application--is-ltr .v-timeline:not(.v-timeline--dense):not(.v-timeline--reverse):before,
 .theme--light.v-timeline::before {
-  border: solid 2px;
+  left: calc(50% - 3px);
+  right: initial;
+  border: solid;
 }
 </style>
