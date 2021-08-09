4aa0d432a2a7f15e60ca9d5fc8483b9a3172fbe0
tkhs <spain3405@gmail.com>
2021-08-09 18:37:37 +0900

    fix resutaurant reserve & style

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -4,7 +4,7 @@
       <div class="text-h5 mb-0 main-color-txt" id="contact">contact</div>
       <div class="text-h4 mb-5">お問い合わせ</div>
       <v-container>
-        <p class="mb-10">制作のご依頼やご相談は、下記の SNS またはクラウドソーシングサイトからお願い致します。</p>
+        <p class="mb-10">制作のご依頼やご相談は、下記のクラウドソーシングサイトまたは SNS からお願い致します。</p>
         <v-row align="center" justify="center">
           <v-btn 
             v-for="item in links" :key="item.name" 
@@ -28,16 +28,6 @@ export default {
       mainColor: '#4dd0e1',
       links: [
         {
-          icon: 'mdi-twitter',
-          name: 'Twitter',
-          url: 'https://twitter.com/aonoweb'
-        },
-        {
-          icon: 'mdi-facebook',
-          name: 'Facebook',
-          url: 'https://www.facebook.com/Aono-Web-Develop-120591110218139/'
-        },
-        {
           icon: 'mdi-web',
           name: 'ココナラ',
           url: 'https://profile.coconala.com/users/1506663'
@@ -51,6 +41,16 @@ export default {
           icon: 'mdi-web',
           name: 'ランサーズ',
           url: 'https://www.lancers.jp/profile/krm50'
+        },
+        {
+          icon: 'mdi-twitter',
+          name: 'Twitter',
+          url: 'https://twitter.com/aonoweb'
+        },
+        {
+          icon: 'mdi-facebook',
+          name: 'Facebook',
+          url: 'https://www.facebook.com/Aono-Web-Develop-120591110218139/'
         },
       ]
     }
