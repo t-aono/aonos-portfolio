e2e4df07db177766ee00d80158cefcf35d238bf1
tkhs <spain3405@gmail.com>
2021-08-12 08:51:35 +0900

    change contact form

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -4,16 +4,13 @@
       <div class="text-h5 mb-0 main-color-txt" id="contact">contact</div>
       <div class="text-h4 mb-5">お問い合わせ</div>
       <v-container>
-        <p class="mb-10">制作のご依頼やご相談は、下記のクラウドソーシングサイトまたは SNS からお願い致します。</p>
         <v-row align="center" justify="center">
-          <v-btn 
-            v-for="item in links" :key="item.name" 
-            :href="item.url" target="_blank" 
-            :color="mainColor" min-width="200" class="ma-3 py-7"
-          >
-            <v-icon x-large color="white">{{ item.icon }}</v-icon>
-            <span class="white--text"> {{ item.name }}</span>
-          </v-btn>
+          <v-col cols="12" md="8">
+            <v-text-field solo label="お名前"></v-text-field>
+            <v-text-field solo label="メールアドレス"></v-text-field>
+            <v-textarea solo label="内容" ></v-textarea>
+            <v-btn large color="cyan lighten-2">送信</v-btn>
+          </v-col>
         </v-row>
       </v-container>
     </v-col>
@@ -22,38 +19,14 @@
 
 <script>
 export default {
-  data: () => {
-    return {
-      subColor: '#314259',
-      mainColor: '#4dd0e1',
-      links: [
-        {
-          icon: 'mdi-web',
-          name: 'ココナラ',
-          url: 'https://profile.coconala.com/users/1506663'
-        },
-        {
-          icon: 'mdi-web',
-          name: 'クラウドワークス',
-          url: 'https://crowdworks.jp/public/employees/1497017'
-        },
-        {
-          icon: 'mdi-web',
-          name: 'ランサーズ',
-          url: 'https://www.lancers.jp/profile/krm50'
-        },
-        {
-          icon: 'mdi-twitter',
-          name: 'Twitter',
-          url: 'https://twitter.com/aonoweb'
-        },
-        {
-          icon: 'mdi-facebook',
-          name: 'Facebook',
-          url: 'https://www.facebook.com/Aono-Web-Develop-120591110218139/'
-        },
-      ]
-    }
-  }
 }
 </script>
+
+<style scoped>
+button.v-btn {
+  text-decoration: none;
+  color: #fff;
+  font-weight: bold;
+  font-size: large;
+}
+</style>
