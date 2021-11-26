b9f2275ae9bf755e1501106d950317b4ab45bfca
tkhs <spain3405@gmail.com>
2021-11-27 08:58:25 +0900

    notion link to maintenance

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -13,7 +13,7 @@
             <v-card-subtitle><v-icon>{{ item.icon }}</v-icon> {{ item.tool }}</v-card-subtitle>
             <v-card-text>{{ item.summary }}</v-card-text>
             <v-card-actions>
-              <v-btn color="cyan lighten-2" :href="item.link" target="_blank">
+              <v-btn color="cyan lighten-2" href="#" onClick="window.alert('メンテナンス中です。');">
                 <span>詳細を見る</span>
               </v-btn>
             </v-card-actions>
@@ -45,81 +45,70 @@ export default {
           icon: "mdi-react",
           tool: "React / TypeScript / Firebase / Chakra UI",
           summary:
-            "ユーザーごとにトレーニングメニューの管理ができ、トレーニングの内容をを記録していくことができるWebアプリ。",
-          link: "https://ossified-elephant-7c0.notion.site/4098f3588edd4490b70355ad5e568d46",
+            "ユーザーごとにトレーニングメニューの管理ができ、トレーニングの内容をを記録していくことができるWebアプリ。"
         },
         {
           title: "画像管理アプリ",
           icon: "mdi-vuejs",
           tool: "Nuxt.js / Firebase / VuexFire / Buefy",
           summary:
-            "ユーザーごとにカテゴリーの管理や、画像の管理ができるWebアプリ。画像は Firebase Storage にアップロードされ一覧表示ができる。カテゴリーごとの絞り込みも可能。",
-          link: "https://ossified-elephant-7c0.notion.site/d70f94a29b3a481a8dfef28366207bae",
+            "ユーザーごとにカテゴリーの管理や、画像の管理ができるWebアプリ。画像は Firebase Storage にアップロードされ一覧表示ができる。カテゴリーごとの絞り込みも可能。"
         },
         {
           title: "観光写真アプリ",
           icon: "mdi-vuejs",
           tool: "Nuxt.js / Firebase / VuexFire / Vuetify",
           summary:
-            "観光地の登録ができ、それに関連する画像を Pixabay API を用いて取得、表示できるWebアプリ。気に入った画像にいいねをすることでトップページに表示することができる。",
-          link: "https://ossified-elephant-7c0.notion.site/8953486949334fbf9c657039dcda7681",
+            "登録した観光地の画像を Pixabay API を用いて取得し表示できる。気に入った画像にいいねをすることでマイページに表示される。"
         },
         {
           title: "レストランの予約アプリ",
           icon: "mdi-vuejs",
           tool: "Vue.js / Vue Router / Firebase / Vutify / SmtpJS / SendGrid",
           summary:
-            "カレンダーの空いているところを選んで予約ができ、管理画面で予約の管理ができる。",
-          link: "https://ossified-elephant-7c0.notion.site/00b7a4b0154041e8826ee86b635e975b",
+            "カレンダーの空いているところを選んで予約ができ、管理画面で予約の管理ができる。"
         },
         {
           title: "商品在庫発注管理アプリ",
           icon: "mdi-vuejs",
           tool: "Vue.js / Vue Router / Vuex / Firebase / Vutify",
           summary:
-            "商品や発注、納入を記録できる。発注や納入の数量に応じて商品の在庫が自動で更新される。",
-          link: "https://ossified-elephant-7c0.notion.site/4126e08c381c4fc089796968540d4c7b",
+            "商品や発注、納入を記録できる。発注や納入の数量に応じて商品の在庫が自動で更新される。"
         },
         {
           title: "アンケートサイト",
           icon: "mdi-vuejs",
           tool: "Vue.js / Vue Router / Vuex / Chart.js / Firebase / BootstrapVue",
           summary:
-            "管理側でアンケートの管理や回答結果の確認ができる。利用者側のアンケートの回答がリアルタイムに結果のグラフに反映される。",
-          link: "https://ossified-elephant-7c0.notion.site/15bbe493583a43608760bddfe2ed6dac",
+            "管理側でアンケートの管理や回答結果の確認ができる。利用者側のアンケートの回答がリアルタイムに結果のグラフに反映される。"
         },
         {
           title: "複数店舗の予約管理アプリ",
           icon: "mdi-laravel",
           tool: "Laravel / SQLite / Tailwind CSS / Stripe",
           summary:
-            "管理者は店舗や予約プランの管理ができ、ユーザーは予約後に決済ができるシステム。",
-          link: "https://ossified-elephant-7c0.notion.site/54f18f6e595447e9be6c072c223bdcbe",
+            "管理者は店舗や予約プランの管理ができ、ユーザーは予約後に決済ができるシステム。"
         },
         {
           title: "タレントプロフィール検索サイト",
           icon: "mdi-laravel",
           tool: "Laravel / SQLite / Tailwind CSS",
           summary:
-            "管理者はタレントプロフィールの登録・変種・削除ができ、ユーザーはタレントの検索ができる。",
-          link: "https://ossified-elephant-7c0.notion.site/eb63168f5d9f4f01af37daea15bacf5e",
+            "管理者はタレントプロフィールの登録・変種・削除ができ、ユーザーはタレントの検索ができる。"
         },
         {
           title: "オンライン講座のシフト管理アプリ",
           icon: "mdi-laravel",
           tool: "Laravel / SQLite / Tailwind CSS",
           summary:
-            "Googleカレンダーのある予定を取得して表示。事前に登録した講師に一括依頼できる。",
-          link: "https://ossified-elephant-7c0.notion.site/72edad3d9b6044c0b99ec16816c31b13",
+            "Googleカレンダーのある予定を取得して表示。事前に登録した講師に一括依頼できる。"
         },
         // {
         //   title: "製造指示共有アプリ",
         //   icon: "mdi-laravel",
         //   tool: "Laravel / SQLite / Tailwind CSS",
         //   summary:
-        //     "指示の一覧表示、詳細表示、登録、更新、削除ができるシステム。一覧表示の絞り込みも可能。",
-        //   link: "https://ossified-elephant-7c0.notion.site/fee7baf7e39b4387a69b79c42bc0fe2e",
-        // }
+        //     "指示の一覧表示、詳細表示、登録、更新、削除ができるシステム。一覧表示の絞り込みも可能。"
       ],
     };
   },
@@ -146,6 +135,13 @@ h3 {
 }
 h2 {
   color: #314259;
+}
+.v-sheet.v-card {
+    height: 280px;
+}
+.v-card__actions {
+    position: absolute;
+    bottom: 16px;
 }
 .v-application a {
   text-decoration: none;
